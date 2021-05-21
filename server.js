2884cef1bab246cba4db774a66e3b517beac1b79
MaxJGruber <maxjgruber@gmail.com>
2021-05-21 10:32:32 +0200

    trying heroku ssl redirect

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const sslRedirect = require("heroku-ssl-redirect");
 const express = require("express");
 
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
+// enable ssl redirect
+app.use(sslRedirect());
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/ping", function (req, res) {
